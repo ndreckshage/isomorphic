faac799aec3a5aff8a5a2ed040f0f9b4f5186573
nick dreckshage <nicholasdreckshage@gmail.com>
2014-08-10 11:06:14 -0400

    critical css support

--- blueprint/app/client.js
+++ blueprint/app/client.js
@@ -1,1 +1,8 @@
-window.isomorphic = require('isomorphic');
+require('isomorphic').isomorphize(window.isomorphize);
+// @TODO improve upon this big time
+window.isomorphicPerformance = function () {
+  var t = window.performance.timing;
+  window.console.log('Server Time: ', t.responseEnd - t.navigationStart, 'ms');
+  window.console.log('Client Time: ', t.domContentLoadedEventEnd - t.domLoading, 'ms');
+  window.console.log('Total Time: ', t.domContentLoadedEventEnd - t. navigationStart, 'ms');
+};
--- blueprint/app/routes/index.js
+++ blueprint/app/routes/index.js
@@ -1,6 +1,7 @@
 var isomorphic = require('isomorphic');
 var TodoListService = isomorphic.require('services/todo-list-service');
 
+module.exports.criticalCSS = 'index';
 module.exports.render = 'index';
 module.exports.promise = function () {
   return TodoListService.fetchIndex();
