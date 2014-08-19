adc2dc5046c340edaabe605de090fd5f36ddb9eb
nick dreckshage <nicholasdreckshage@gmail.com>
2014-08-18 23:28:43 -0400

    forgot to pull out proxy

--- blueprint/base/server.js
+++ blueprint/base/server.js
@@ -2,22 +2,13 @@ var express = require('express');
 var isomorphic = require('isomorphic');
 var app = express();
 
+var PORT = process.env.PORT || 3030;
+var API_MOUNT = process.env.API_MOUNT || '/api';
+
+// @TODO move somewhere
 require('node-jsx').install({extension: '.jsx'});
+
 app.use(express.static(__dirname + '/public'));
-
 app.use(isomorphic.router.middleware());
-// app.use(isormophic.router())
-
-// @TODO temp
-isomorphic.proxy = function () {
-  var request = require('request');
-  return function (req, res) {
-    req.pipe(request('http://isomorphic-todo-api.frontendperformance.com' + req.url)).pipe(res)
-  }
-}
-
-app.use('/api', isomorphic.proxy());
-// app.use(API_MOUNT, isomorphic.proxy())
-
-var port = process.env.PORT || 3030;
-app.listen(port);
+app.use(API_MOUNT, isomorphic.request.proxy());
+app.listen(PORT);
